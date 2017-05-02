dd8adf988015f2308cfff4ddbc05207050dfb640
kmelve <knut@netliferesearch.com>
2017-05-02 21:54:53 +0200

    proof of concept

--- src/containers/ArticlePage/components/Article.jsx
+++ src/containers/ArticlePage/components/Article.jsx
@@ -6,24 +6,61 @@
  *
  */
 
-import React, { PropTypes, Component } from 'react';
-
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 import {
   initArticleScripts,
   removeEventListenerForResize,
   removeAsideClickListener } from 'ndla-article-scripts';
 import getLicenseByAbbreviation from 'ndla-licenses';
+import SelectionPopover from 'react-selection-popover'
 import { Article as UIArticle, OneColumn } from '../../../../lib';
 import { injectT } from '../../../i18n';
 import ToggleLicenseBox from './ToggleLicenseBox';
 import LicenseBox from '../../../components/license/LicenseBox';
 import { SubjectShape, TopicShape } from '../../../shapes';
-
+const overlay = {
+  position: 'fixed',
+  height: '100%',
+  width: '100%',
+  background: 'rgba(206,221,234,0.5)',
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+  zIndex: 100,
+  display: 'flex',
+  alignItems: 'center'
+}
 
 class Article extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      showPopover: false,
+      explainator: false,
+      selectedText: null
+    }
+  }
 
   componentDidMount() {
     initArticleScripts();
+    //console.log('test')
+    document.addEventListener("selectionchange", event => {
+      const selection = encodeURI(window.getSelection().toString().split(' ')[0])
+      fetch(`https://snl.no/api/v1/search?query=${selection}&limit=5&offset=0`).then(data => data.json())
+        .then(data => this.setState({wikiResponse: data}))
+      // selection.baseNode.textContent.replace('/yrker/i', '<span class="test">yrker</span>')
+      /*
+      var index = innerHTML.indexOf('yrker');
+      console.dir(selection)
+      if ( index >= 0 )
+      {
+        innerHTML = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
+        inputText.innerHTML = innerHTML
+      }
+      */
+    });
   }
 
   componentWillUnmount() {
@@ -53,6 +90,13 @@ class Article extends Component {
     );
   }
 
+  toggleExplainator() {
+    this.setState({
+      selectedText: window.getSelection().toString(),
+      explainator: !this.state.explainator
+    })
+  }
+
   render() {
     const { article } = this.props;
 
@@ -65,11 +109,39 @@ class Article extends Component {
         <OneColumn cssModifier="narrow">
           {this.renderToggleLicenseBox(true)}
         </OneColumn>
-        <div className="c-article--narrow" dangerouslySetInnerHTML={{ __html: article.content }} />
-        <section>
-          {this.renderToggleLicenseBox()}
-          <a className="article-old-ndla-link" rel="noopener noreferrer" target="_blank" href={article.oldNdlaUrl}>Gå til orginal artikkel</a>
-        </section>
+        <div style={{position: 'relative'}}>
+          <div data-selectable className="c-article--narrow" dangerouslySetInnerHTML={{ __html: article.content }} />
+          <section>
+            {this.renderToggleLicenseBox()}
+            <a className="article-old-ndla-link" rel="noopener noreferrer" target="_blank" href={article.oldNdlaUrl}>Gå til orginal artikkel</a>
+          </section>
+          <SelectionPopover
+            showPopover={this.state.showPopover}
+            onSelect={() => {this.setState({showPopover: true})}}
+            onDeselect={() => {this.setState({showPopover: false})}}
+          >
+            <button style={{padding: 3, lineHeight: 1}} className='o-button c-button' onClick={() => this.toggleExplainator() }>Forklar begrep(er)</button>
+            <button style={{padding: 3, lineHeight: 1}} className='o-button c-button'>Les høyt</button>
+            <button style={{padding: 3, lineHeight: 1}} className='o-button c-button'>Lagre utdrag</button>
+          </SelectionPopover>
+        </div>
+        <div>
+
+        </div>
+          {this.state.explainator && (
+          <div style={overlay}>
+            <div style={{background: '#fff', position: 'relative', maxWidth: 900, minWidth: 200, minHeight: 200, margin: '0 auto'}}>
+              <button className='o-button c-button' onClick={() => this.toggleExplainator()}>Lukk</button>
+              <ul className='o-list--arrows'>
+                Du markerte: {this.state.selectedText.split(' ').map((word, index) => `${word} ${index > 1 ? ', ' : ''}`)}
+                <ul>Søketreff fra SNL
+                {this.state.wikiResponse && this.state.wikiResponse.map(word => <li><strong>{word.headword}</strong>: {word['first_two_sentences']}</li>)}
+                </ul>
+              </ul>
+            </div>
+          </div>
+          )
+          }
       </section>
     );
   }

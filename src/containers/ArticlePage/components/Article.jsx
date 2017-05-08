d8238a6525841d3d167d83a4dbbbfaa09e8740c3
thomaslgj <thomas@netliferesearch.com>
2017-05-08 13:43:10 +0200

    New article code from knowit

--- src/containers/ArticlePage/components/Article.jsx
+++ src/containers/ArticlePage/components/Article.jsx
@@ -6,61 +6,25 @@
  *
  */
 
-import React, { Component } from 'react';
-import PropTypes from 'prop-types'
+import React, { PropTypes, Component } from 'react';
+
+import { Article as UIArticle, LicenseByline, TopicBreadcrumb, OneColumn } from 'ndla-ui';
 import {
   initArticleScripts,
   removeEventListenerForResize,
   removeAsideClickListener } from 'ndla-article-scripts';
 import getLicenseByAbbreviation from 'ndla-licenses';
-import SelectionPopover from 'react-selection-popover'
-import { Article as UIArticle, OneColumn } from '../../../../lib';
+import { toTopic } from '../../../routes';
 import { injectT } from '../../../i18n';
 import ToggleLicenseBox from './ToggleLicenseBox';
 import LicenseBox from '../../../components/license/LicenseBox';
 import { SubjectShape, TopicShape } from '../../../shapes';
-const overlay = {
-  position: 'fixed',
-  height: '100%',
-  width: '100%',
-  background: 'rgba(206,221,234,0.5)',
-  top: 0,
-  bottom: 0,
-  left: 0,
-  right: 0,
-  zIndex: 100,
-  display: 'flex',
-  alignItems: 'center'
-}
+
 
 class Article extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      showPopover: false,
-      explainator: false,
-      selectedText: null
-    }
-  }
 
   componentDidMount() {
     initArticleScripts();
-    //console.log('test')
-    document.addEventListener("selectionchange", event => {
-      /*
-      const selection = encodeURI(window.getSelection().toString().split(' ')[0])
-      fetch(`https://snl.no/api/v1/search?query=${selection}&limit=5&offset=0`).then(data => data.json())
-        .then(data => this.setState({wikiResponse: data}))
-
-      var index = innerHTML.indexOf('yrker');
-      console.dir(selection)
-      if ( index >= 0 )
-      {
-        innerHTML = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
-        inputText.innerHTML = innerHTML
-      }
-      */
-    });
   }
 
   componentWillUnmount() {
@@ -81,67 +45,38 @@ class Article extends Component {
         licenseBox={<LicenseBox article={article} locale={locale} license={license} />}
       >
         { showByline ?
-          <div className="c-article-meta">
-            <span className="c-article__metatext">{authorsList}. Publisert: {article.created}</span>.
-          </div>
+          <LicenseByline license={license}>
+            <span className="article_meta">{authorsList}. Publisert: {article.created}</span>.
+          </LicenseByline>
           : null
         }
       </ToggleLicenseBox>
     );
   }
 
-  toggleExplainator() {
-    this.setState({
-      selectedText: window.getSelection().toString(),
-      explainator: !this.state.explainator
-    })
-  }
-
   render() {
-    const { article } = this.props;
+    const { article, subject, topicPath } = this.props;
 
     return (
       <section className="c-article-content">
-
-        <h1 className="c-article__title">{article.title}</h1>
-        <UIArticle.Introduction introduction={article.introduction} />
-        { article.footNotes ? <UIArticle.FootNotes footNotes={article.footNotes} /> : null }
         <OneColumn cssModifier="narrow">
-          {this.renderToggleLicenseBox(true)}
-        </OneColumn>
-        <div style={{position: 'relative'}}>
-          <div data-selectable className="c-article--narrow" dangerouslySetInnerHTML={{ __html: article.content }} />
           <section>
+            { subject ? <TopicBreadcrumb toSubjects={() => '/'} subjectsTitle="Fag" subject={subject} topicPath={topicPath} toTopic={toTopic}>Du er her:</TopicBreadcrumb> : null }
             {this.renderToggleLicenseBox()}
+            <h1>{article.title}</h1>
+            <UIArticle.Introduction introduction={article.introduction} />
+          </section>
+        </OneColumn>
+        <OneColumn cssModifier="narrow">
+          <div dangerouslySetInnerHTML={{ __html: article.content }} />
+        </OneColumn>
+        <OneColumn cssModifier="narrow">
+          <section>
+            { article.footNotes ? <UIArticle.FootNotes footNotes={article.footNotes} /> : null }
+            {this.renderToggleLicenseBox(true)}
             <a className="article-old-ndla-link" rel="noopener noreferrer" target="_blank" href={article.oldNdlaUrl}>Gå til orginal artikkel</a>
           </section>
-          <SelectionPopover
-            showPopover={this.state.showPopover}
-            onSelect={() => {this.setState({showPopover: true})}}
-            onDeselect={() => {this.setState({showPopover: false})}}
-          >
-            <button style={{padding: 3, lineHeight: 1}} className='o-button c-button' onClick={() => this.toggleExplainator() }>Forklar begrep(er)</button>
-            <button style={{padding: 3, lineHeight: 1}} className='o-button c-button'>Les høyt</button>
-            <button style={{padding: 3, lineHeight: 1}} className='o-button c-button'>Lagre utdrag</button>
-          </SelectionPopover>
-        </div>
-        <div>
-
-        </div>
-          {this.state.explainator && (
-          <div style={overlay}>
-            <div style={{background: '#fff', position: 'relative', maxWidth: 900, minWidth: 200, minHeight: 200, margin: '0 auto'}}>
-              <button className='o-button c-button' onClick={() => this.toggleExplainator()}>Lukk</button>
-              <ul className='o-list--arrows'>
-                Du markerte: {this.state.selectedText.split(' ').map((word, index) => `${word} ${index > 1 ? ', ' : ''}`)}
-                <ul>Søketreff fra SNL
-                {this.state.wikiResponse && this.state.wikiResponse.map(word => <li><strong>{word.headword}</strong>: {word['first_two_sentences']}</li>)}
-                </ul>
-              </ul>
-            </div>
-          </div>
-          )
-          }
+        </OneColumn>
       </section>
     );
   }

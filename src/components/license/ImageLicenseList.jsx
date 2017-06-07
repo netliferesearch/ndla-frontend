fe9fb899e7ccbaf09c4e3388510ab3bc335465dc
thomaslgj <thomas@netliferesearch.com>
2017-06-07 12:22:03 +0200

    Fix typo in license box label

--- src/components/license/ImageLicenseList.jsx
+++ src/components/license/ImageLicenseList.jsx
@@ -31,7 +31,7 @@ const ImageLicenseInfo = ({ image, locale }) => (
       title="Regler for bruk av bildet"
     >
       <MediaListItemActions>
-        <h3 className="c-medialist__title">Slik skal du referere til teksten</h3>
+        <h3 className="c-medialist__title">Slik skal du referere til bildet</h3>
         <MediaListItemMeta authors={image.copyright.authors} />
         <button className="c-button c-button--outline c-licenseToggle__button" type="button">Kopier referanse</button>
         <button className="c-button c-button--outline c-licenseToggle__button" type="button">Vis bilde</button>

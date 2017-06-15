7af0c4d0bfc002ba4c6c04d7bdc3309ba23ecadb
kmelve <knut@netliferesearch.com>
2017-06-15 21:14:58 +0200

    put article converter url in enviroment, fallback on localhost

--- src/containers/ArticlePage/articleApi.js
+++ src/containers/ArticlePage/articleApi.js
@@ -13,8 +13,9 @@ import {
   headerWithAccessToken,
 } from '../../util/apiHelpers';
 
-const converterBaseUrl = 'https://ndla-article-converter.herokuapp.com/article-converter/raw';
-//const converterBaseUrl = 'http://localhost:3100/article-converter/raw';
+const converterBaseUrl =
+  process.env.ARTICLE_CONVERTER_URL ||
+  'http://localhost:3100/article-converter/raw';
 const baseUrl = apiResourceUrl('/article-api/v1/articles');
 
 export const fetchArticle = (id, locale, token) =>

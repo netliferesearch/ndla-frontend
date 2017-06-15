4736b1f836e428e9ca90a1d4cb5e39c20efe0cfc
kmelve <knut@netliferesearch.com>
2017-06-15 14:53:14 +0200

    fix article converter

--- src/containers/ArticlePage/articleApi.js
+++ src/containers/ArticlePage/articleApi.js
@@ -13,8 +13,8 @@ import {
   headerWithAccessToken,
 } from '../../util/apiHelpers';
 
-//const converterBaseUrl = 'https://ndla-article-converter.herokuapp.com/article-converter/raw';
-const converterBaseUrl = 'http://localhost:3100/article-converter/raw';
+const converterBaseUrl = 'https://ndla-article-converter.herokuapp.com/article-converter/raw';
+//const converterBaseUrl = 'http://localhost:3100/article-converter/raw';
 const baseUrl = apiResourceUrl('/article-api/v1/articles');
 
 export const fetchArticle = (id, locale, token) =>
